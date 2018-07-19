1ee11691d9290b746143d02c61e0d294557faf55
sobhanwrc <sobhan@wrctpl.com>
2018-07-19 10:43:09 +0530

    commit by sobhan

--- routes/api/index.js
+++ routes/api/index.js
@@ -12,6 +12,9 @@ const cleanCache = require('../../middlewares/cleanCache');
 const User = require('../../models/User').User;
 const Industry = require('../../models/Industry').Industry;
 const Admin = require('../../models/Admin').Admin;
+var fs = require('fs');
+var multer = require('multer');
+var base64ToImage = require('base64-to-image');
 //for sending email
 // const sendmail = require('sendmail')();
 const Mailjet = require('node-mailjet').connect('f6419360e64064bc8ea8c4ea949e7eb8', 'fde7e8364b2ba00150f43eae0851cc85');
@@ -495,6 +498,49 @@ router.post('/otp-verify-and-update-pw', async (req,res) => {
       message: "OTP does not match."
     });
   }
+});
+
+
+var image = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/app_profile_image');
+  },
+  filename: function (req, file, cb) {
+    fileExt = file.mimetype.split('/')[1];
+    
+    fileName = req.user.id + '-' + Date.now() + '.' + fileExt;
+    cb(null, fileName);
+  }
+});
+
+var profile_image_upload = multer({ storage: image, limits: {fileSize:3000000} });
+
+router.post('/profile/image-upload', passport.authenticate('jwt', {session: false}), async (req,res) => {
+  var user = await User.findOne({_id: req.user.id});
+  
+  var image_file = req.body.profile_image;
+  console.log('writing file...', image_file);
+
+  // fs.writeFile("/public/app_profile_image/out.png", image_file, 'base64', function(err) {
+  //     if (err) console.log(err);
+  //     fs.readFile("/public/app_profile_image/out.png", function(err, data) {
+  //         if (err) throw err;
+  //         console.log('reading file...', data.toString('base64'));
+  //         res.send(data);
+  //     });
+  // });
+
+  var base64Str = req.body.profile_image;
+  var path ='public/app_profile_image/';
+
+  var imageFileName = req.user.id + '-' + Date.now();
+  var optionalObj = {'fileName': imageFileName, 'type':'jpg'};
+
+      // base64ToImage(base64Str,path,optionalObj); 
+      
+  // Note base64ToImage function returns imageInfo which is an object with imageType and fileName.
+  var imageInfo = base64ToImage(base64Str,path,optionalObj); 
+  console.log(imageInfo);
 });
 
 
