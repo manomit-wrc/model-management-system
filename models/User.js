e6196d46daafc3d9a959a90f2840b061fb953e53
sobhanwrc <sobhan@wrctpl.com>
2018-08-02 14:04:47 +0530

    commit by sobhan

--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@ const { Schema } = mongoose;
 
 const userSchema = new Schema({
     first_name: { type: String, required: true },
-    last_name: { type: String, required: true },
+    last_name: { type: String},
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     avatar: { type: String },
