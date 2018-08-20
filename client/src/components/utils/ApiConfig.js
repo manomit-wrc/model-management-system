6d6478444ed9efebac053a161efa60ab0fd29c5f
Manomit Mitra <manomit@wrctechnologies.com>
2018-08-20 13:16:55 +0530

    commit

--- client/src/components/utils/ApiConfig.js
+++ client/src/components/utils/ApiConfig.js
@@ -4,7 +4,7 @@ if(hostname === 'localhost') {
     backendHost = 'http://localhost:8000/api/users';
 } 
 else {
-    backendHost = 'http://mms.wrctpl.com/api/users';
+    backendHost = 'http://localhost:8000/api/users';
 }
 
 export const API_ROOT = `${backendHost}`;
